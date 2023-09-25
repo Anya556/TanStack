e8d242aa8d89b8e82e43bc9fa7fc6e063f154548
Anna <gstudentseo@gmail.com>
2023-09-25 22:38:52 +0200

    Made the first request to the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import './App.css';
+import { useQuery } from '@tanstack/react-query';
+
+const todoId = 1;
 
 function App() {
-
-  return (
-   
-      <div className="App"></div>
-    
-)}
+  const { isLoading, error, data } = useQuery(
+    ['todos', todoId],
+    () =>
+      fetch('https://jsonplaceholder.typicode.com/todos/1').then((response) =>
+        response.json()
+      )
+  );
+  return <div>{data ? <h1>Todo: {data.title}</h1> : <h1>Data not found!</h1>}</div>;
+}
 
 export default App;
