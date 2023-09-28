1241b82248a30c9df3e83db4898cc35ced8181ee
Anna <gstudentseo@gmail.com>
2023-09-28 22:20:29 +0200

    Using invalidateQueries

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { useTodos } from './hooks/useTodos';
+import { useQueryClient } from '@tanstack/react-query';
 
 function App() {
   const { isLoading, data } = useTodos();
-  
+  const queryClient = useQueryClient();
+
   return (
     <div>
+      <button onClick={() => queryClient.invalidateQueries(['todos'])}>
+        Refresh
+      </button>
+
       {isLoading ? (
         <div>Loading....</div>
       ) : data?.length ? (
         data.map((todo) => (
-          <div>
+          <div key={todo.id}>
             <b>{todo.id}:</b> {todo.title}
           </div>
         ))
--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -4,11 +4,5 @@ import todoService from '../services/todo.service';
 export const useTodos = () => {
   return useQuery(['todos'], () => todoService.getAll(), {
     select: ({ data }) => data,
-    // onSuccess(data) {
-    //   alert(data[3].title)
-    // },
-    // onError(err) {
-    //   alert(err)
-    // }
   });
 };
